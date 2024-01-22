d78e733586ef75e504062fec7de4ebbf14ee8125
Jacob Sendzik <jake.sendzik@gmail.com>
2024-01-22 12:32:01 -0500

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = []
+    for n in num_list:
+        if n % 2 == 0:
+            evens.append(n)
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    sent = []
+    if len(sentence_list) == 0:
+        return sent
+    else:
+        for s in sentence_list:
+            sent.append(s + "!")
+        return sent
